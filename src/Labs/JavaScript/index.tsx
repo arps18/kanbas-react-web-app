28b0e87bf5ec1600c9cb392717a5a40ac8576cc5
arps <arpanp99@gmail.com>
2024-02-20 17:05:28 -0500

    2.2.3 added

--- src/Labs/JavaScript/index.tsx
+++ src/Labs/JavaScript/index.tsx
@@ -1,9 +1,14 @@
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+import VariableTypes from "./variables/VariableTypes";
+
 function JavaScript() {
     console.log("hello World!");
     
     return(
        <div>
           <h1>JavaScript</h1>
+          <VariablesAndConstants />
+          <VariableTypes />
        </div>
     );
  }
