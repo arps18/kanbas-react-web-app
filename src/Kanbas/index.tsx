af4b3ae8501a24149cda433e6678fcca733d99ba
arps <arpanp99@gmail.com>
2024-02-21 17:40:03 -0500

    Kanbas started

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,13 +1,19 @@
 import {Link} from "react-router-dom";
 import Nav from "../Nav";
-function Kanbas(){
+function Kanbas() {
     return (
+      <div className="d-flex">
         <div>
-            <Nav />
-            <h1>Kanbas</h1>
+          <h1>Kanbas Navigation</h1>
         </div>
-    );
-}
+        <div style={{ flexGrow: 1 }}>
+          <h1>Account</h1>
+          <h1>Dashboard</h1>
+          <h1>Courses</h1>
+        </div>
+      </div>
+  );}
+ 
 
 export default Kanbas;
 
