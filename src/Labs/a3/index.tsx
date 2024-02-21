a804340065e151e1478c3891806963694e3a1cda
arps <arpanp99@gmail.com>
2024-02-21 15:57:10 -0500

    2.4  added

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,9 +1,14 @@
 import JavaScript from "../JavaScript";
+import Classes from "./Classes";
+import PathParameters from "./routing/PathParameters";
+
 
 function Assignment3() {
     return (
       <div className="container">
         <h1>Assignment 3</h1>
+        <Classes />
+        <PathParameters />
         <JavaScript />
       </div>
     );
