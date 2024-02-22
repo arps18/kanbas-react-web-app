4c97e0b19277b1cfcea369ca4a82af29a933f79d
arps <arpanp99@gmail.com>
2024-02-21 21:32:49 -0500

    3.3 added

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,5 +1,7 @@
 import {Link} from "react-router-dom";
 import Nav from "../Nav";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 function Kanbas() {
     return (
@@ -9,9 +11,13 @@ function Kanbas() {
           <h1>Kanbas Navigation</h1>
         </div>
         <div style={{ flexGrow: 1 }}>
-          <h1>Account</h1>
-          <h1>Dashboard</h1>
-          <h1>Courses</h1>
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account" element={<h1>Account</h1>} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/*" element={<h1>Courses</h1>} />
+        </Routes>
+
         </div>
       </div>
   );}
