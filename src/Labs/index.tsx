3dfffc79bcde24d34b7c934489ef95436aab600b
arps <arpanp99@gmail.com>
2024-02-22 20:35:09 -0500

    Lab issue fixed

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Nav from '../Nav';
 
 import Assignment3 from './a3';
+import Assignment4 from './a4';
 function Labs() {
     return (
       <div className="container-fluid">
@@ -13,7 +14,7 @@ function Labs() {
         <Link to="/Labs/a4">Assignment 4</Link>
         <Routes>
         <Route path="/a3/*" element={<Assignment3 />} />
-        {/* <Route path="/a4" element={<Assignment4 />} /> */}
+        <Route path="/a4" element={<Assignment4 />} />
       </Routes>
     </div>
   );
