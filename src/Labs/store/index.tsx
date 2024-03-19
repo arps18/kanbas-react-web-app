610d584e2843ee72a8462f5c5f1243ab7eb04dde
arps <arpanp99@gmail.com>
2024-03-19 14:47:34 -0400

    2.4.3

--- src/Labs/store/index.tsx
+++ src/Labs/store/index.tsx
@@ -1,13 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../a4/ReduxExamples/HelloRedux/helloReducer";
+import counterReducer from "../a4/ReduxExamples/CounterRedux/counterReducer";
+
 export interface LabState {
   helloReducer: {
     message: string;
-  };
+};
+counterReducer: {
+  count: number;
+};
 }
 const store = configureStore({
   reducer: {
     helloReducer,
+    counterReducer,
   },
 });
 export default store;
